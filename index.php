7851158c03146071c01292fb18454a4a389e6656
zayceva-nastya <59017854+zayceva-nastya@users.noreply.github.com>
2019-12-24 20:01:33 +0300

    add php

--- index.php
+++ index.php
@@ -5,15 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <style>
-   body{
-    
-        padding: 0 20%;
-    }
-        
-        </style>
 </head>
 <body>
-    <h1>Hello, World</h1>
+<?php
+   echo 'Hello, World';
+   ?>
 </body>
 </html>
